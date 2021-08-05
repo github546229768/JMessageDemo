fa0717145e871f5fc71eaf8aac041b2043ca0646
nickacy <2591796467@qq.com>
2021-08-05 10:12:30 +0800

    修改App类名

--- app/src/main/java/com/rl/jmessagedemo/MyApplication.kt
+++ app/src/main/java/com/rl/jmessagedemo/MyApplication.kt
@@ -13,7 +13,7 @@ import com.blankj.utilcode.util.Utils
  * @desc:
 
  */
-class MyApplication : Application() {
+class App : Application() {
     override fun onCreate() {
         super.onCreate()
         Utils.init(this)
